e82105d0bbfbd4b276c72d4dbfa17f5270cd06f3
doantam123 <doantampn2017@gmail.com>
2024-09-13 21:53:02 +0700

    done static FE

--- frontend/src/components/ProductCarouselComponent.js
+++ frontend/src/components/ProductCarouselComponent.js
@@ -9,7 +9,7 @@ const ProductCarouselComponent = () => {
     <Carousel>
       <Carousel.Item>
         <img
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
           className="d-block w-100"
           style={{ height: "300px", objectFit: "cover" }}
           src="/images/carousel/carousel-1.png"
@@ -24,6 +24,7 @@ const ProductCarouselComponent = () => {
       </Carousel.Item>
       <Carousel.Item>
         <img
+          crossOrigin="anonymous"
           className="d-block w-100"
           style={{ height: "300px", objectFit: "cover" }}
           src="/images/carousel/carousel-2.png"
@@ -39,6 +40,7 @@ const ProductCarouselComponent = () => {
       </Carousel.Item>
       <Carousel.Item>
         <img
+          crossOrigin="anonymous"
           className="d-block w-100"
           style={{ height: "300px", objectFit: "cover" }}
           src="/images/carousel/carousel-3.png"
