f407f5b87becc251fe6155cddafdae7f95b466a5
doantam123 <doantampn2017@gmail.com>
2024-09-15 15:36:11 +0700

    add db

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const connectDb = require('./config/db');  // Correct import
+
+const app = express();
+const port = 3000;
+
+app.use((error, req, res, next) => {
+    res.status(500).json({
+        message: error.message,
+        stack: error.stack
+    });
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(port, async () => {
+    await connectDb();  // Make sure this works after correct import
+    console.log(`Example app listening on port ${port}`);
+});
