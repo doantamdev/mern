5edf831b6a54dea31a5b12b6f125cc4967260e12
doantam123 <doantampn2017@gmail.com>
2024-11-11 22:11:42 +0700

    add func remove cart

--- frontend/src/pages/components/CartPageComponent.js
+++ frontend/src/pages/components/CartPageComponent.js
@@ -4,43 +4,57 @@ import CartItemComponent from "../../components/CartItemComponent";
 
 const CartPageComponent = ({
   addToCart,
+  removeFromCart,
   cartItems,
   cartSubtotal,
   reduxDispatch,
 }) => {
-  console.log(cartItems);
+  const changeCount = (productID, count) => {
+    reduxDispatch(addToCart(productID, count));
+  };
+
+  const removeFromCartHandler = (productID, quantity, price) => {
+    if (window.confirm("Are you sure?")) {
+      reduxDispatch(removeFromCart(productID, quantity, price));
+    }
+  };
+
   return (
     <Container fluid>
       <Row className="mt-4">
         <Col md={8}>
           <h1>Shopping Cart</h1>
-          <ListGroup variant="flush">
-            {Array.from({ length: 3 }).map((item, idx) => (
-              <CartItemComponent
-                item={{
-                  image: { path: "/images/tablets-category.png" },
-                  name: "Product name",
-                  price: 10,
-                  count: 10,
-                  quantity: 10,
-                }}
-                key={idx}
-              />
-            ))}
-          </ListGroup>
-          <Alert variant="info">Your cart is empty</Alert>
+          {cartItems.length === 0 ? (
+            <Alert variant="info">Your cart is empty</Alert>
+          ) : (
+            <ListGroup variant="flush">
+              {cartItems.map((item, idx) => (
+                <CartItemComponent
+                  item={item}
+                  key={idx}
+                  changeCount={changeCount}
+                  removeFromCartHandler={removeFromCartHandler}
+                />
+              ))}
+            </ListGroup>
+          )}
         </Col>
         <Col md={4}>
           <ListGroup>
             <ListGroup.Item>
-              <h3>Subtotal (2 Items)</h3>
+              <h3>
+                Subtotal ({cartItems.length}{" "}
+                {cartItems.length === 1 ? "Product" : "Products"})
+              </h3>
             </ListGroup.Item>
             <ListGroup.Item>
-              Price: <span className="fw-bold">$892</span>
+              Price: <span className="fw-bold">${cartSubtotal}</span>
             </ListGroup.Item>
             <ListGroup.Item>
               <LinkContainer to="/user/cart-details">
-                <Button type="button">Proceed To Checkout</Button>
+                <Button disabled={cartSubtotal === 0} type="button">
+                  Proceed To Checkout
+                </Button>
               </LinkContainer>
             </ListGroup.Item>
           </ListGroup>
