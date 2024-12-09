3a50f6bbcb2ddfaa054fad2b5eb3b5fb73292f6e
doantam123 <doantampn2017@gmail.com>
2024-12-09 22:39:17 +0700

    add pagination

--- frontend/src/pages/ProductListPage.js
+++ frontend/src/pages/ProductListPage.js
@@ -3,8 +3,21 @@ import axios from "axios";
 
 import { useSelector } from "react-redux";
 
-const getProducts = async () => {
-  const { data } = await axios.get("/api/products");
+let filtersUrl = "";
+
+const getProducts = async (
+  categoryName = "",
+  pageNumParam = null,
+  searchQuery = "",
+  filters = {},
+  sortOption = ""
+) => {
+  filtersUrl = "";
+  console.log(filters);
+  const search = searchQuery ? `search/${searchQuery}/` : "";
+  const category = categoryName ? `category/${categoryName}/` : "";
+  const url = `/api/products/${category}${search}?pageNum=${pageNumParam}${filtersUrl}&sort=${sortOption}`;
+  const { data } = await axios.get(url);
   return data;
 };
 
